d77d670ac4b921a00f88c9b2e37392a7fcf10561
川野辺 亮 <gggnbr@gmail.com>
2024-06-19 22:06:09 +0900

    fix: コード表示部分でエラーになったのでコメントアウト

--- src/app/image_tag/components/hero/hero.tsx
+++ src/app/image_tag/components/hero/hero.tsx
@@ -6,12 +6,12 @@ export default function Hero() {
   return (
     <>
       <Image className="mb-4" src={img} placeholder="blur" alt=""></Image>
-      <pre className="px-4 py-4 bg-slate-800 rounded-xl text-slate-300 font-light">
+      {/* TODO: コード表示 <pre className="px-4 py-4 bg-slate-800 rounded-xl text-slate-300 font-light">
         <code>
           &lt;Image src=&#123;img&#125; placeholder="blur"
           alt=""&gt;&lt;/Image&gt;
         </code>
-      </pre>
+  </pre>*/}
     </>
   );
 }
