fbed06e2f4eacef282b213ba9aabb141bab06ac5
川野辺 亮 <gggnbr@gmail.com>
2024-06-19 22:02:33 +0900

    feat: ホームから各サンプルへリンクするようにした

---
+++ src/app/components/navlink/navlink.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import React from "react";
+
+export default function NavLink({
+  href,
+  children,
+}: {
+  href: string;
+  children: Readonly<React.ReactNode>;
+}) {
+  return (
+    <Link
+      className="px-4 py-2 border border-slate-800 rounded-lg hover:border-slate-500 hover:bg-slate-800 duration-150"
+      href={href}
+    >
+      {children}
+    </Link>
+  );
+}
